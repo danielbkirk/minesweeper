b2a0040f43d2359edebbf07ce0b447bf23a970ce
Daniel Kirk <danielkrk2@gmail.com>
2022-05-18 14:53:20 +0100

    Added Jacoco Reports

---
+++ src/test/java/TileTest.java
@@ -0,0 +1,12 @@
+import oop.blueprints.Tile;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TileTest {
+    Tile testTile = new Tile(true);
+
+    @Test
+    public void testHasMine(){
+        Assertions.assertEquals(true, testTile.assignedMine(), "This does not have a mine assigned to it");
+    }
+}
